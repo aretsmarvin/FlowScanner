de19234bd84195424178dd9ffbf3a88b9b1264df
Marvin Arets <mail@marvinarets.eu>
2022-03-09 15:47:14 +0100

    Seperate TCP/UDP and make scans working

--- FlowScanner/Tools/Scans.py
+++ FlowScanner/Tools/Scans.py
@@ -5,41 +5,67 @@ Module to perform the scans
 from multiprocessing.pool import ThreadPool
 import subprocess
 
-class Scans:
+def PerformScans(server_list) -> None:
     """
-    The Scans class is responsible for performing the actual scans. It uses
-    subprocesses to run multiple scans simultaneously
+    TODO: fill this
     """
+    num = None
+    thread_pool = ThreadPool(num)
+    for server in server_list:
+        thread_pool.apply_async(ScanWorker,
+                                (server.get('ip_version'),
+                                    server.get('ipaddress'),
+                                    server.get('portlist_tcp'),
+                                    server.get('portlist_udp'),))
 
-    def Perform(self, server_list) -> None:
-        """
-        TODO: fill this
-        """
-        num = None
-        thread_pool = ThreadPool(num)
-        for server in server_list:
-            thread_pool.apply_async(self.ScanWorker,
-                                    (server.get('ip_version'),
-                                       server.get('ipaddress'),
-                                       server.get('portlist'),))
-
-        thread_pool.close()
-        thread_pool.join()
+    thread_pool.close()
+    thread_pool.join()
 
 
-    def ScanWorker(self, ip_version, ip_address, port_list):
-        """
-        TODO: fill this
-        """
-        command = ['nmap', str(ip_address), '-p', ','.join(port_list), '-Pn']
-        if ip_version == "IPv6":
-            command.append('-6')
+def ScanWorker(ip_version, ip_address, port_list_tcp, port_list_udp):
+    """
+    TODO: fill this
+    """
+    if 'None' in port_list_tcp:
+        port_list_tcp.remove('None')
+    if 'None' in port_list_udp:
+        port_list_udp.remove('None')
 
-        with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as sub:
-            sub.wait()
-            print(sub)
-            for stdout_line in iter(sub.stdout.readline, ""):
-                if stdout_line:
-                    print(stdout_line)
-                else:
-                    return
+    if port_list_tcp:
+        NmapTCPScan(ip_version, ip_address, ','.join(port_list_tcp))
+
+    if port_list_udp:
+        NmapUDPScan(ip_version, ip_address, ','.join(port_list_udp))
+
+def NmapTCPScan(ip_version, ip_address, port_list):
+    """
+    TODO: fill this
+    """
+    command = ['nmap',
+                str(ip_address),
+                '-p', port_list,
+                '-Pn',
+                '-oX',
+                '/home/marvin/Bureaublad/FlowScanner/Files/output.xml',
+                '--append-output']
+    if ip_version == "IPv6":
+        command.append('-6')
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as sub:
+        sub.wait()
+
+def NmapUDPScan(ip_version, ip_address, port_list):
+    """
+    TODO: fill this
+    """
+    command = ['nmap',
+                str(ip_address),
+                '-p', ','.join(port_list),
+                '-Pn',
+                '-sU',
+                '-oX',
+                '/home/marvin/Bureaublad/FlowScanner/Files/output.xml',
+                '--append-output']
+    if ip_version == "IPv6":
+        command.append('-6')
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as sub:
+        sub.wait()
