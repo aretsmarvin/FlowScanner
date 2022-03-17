b26f98949449e75d3edbba0500ec597b6ad232f7
Marvin Arets <mail@marvinarets.eu>
2022-03-17 14:12:43 +0100

    More scan options

--- FlowScanner/Tools/Scans.py
+++ FlowScanner/Tools/Scans.py
@@ -38,6 +38,25 @@ def ScanWorker(ip_version, ip_address, port_list_tcp, port_list_udp):
     if port_list_udp:
         NmapUDPScan(ip_version, ip_address, ','.join(port_list_udp))
 
+    command = ['ivre',
+                'scan2db',
+                '-c',
+                'netflow',
+                '-r',
+                os.getenv('nmap_tmp_output_folder') + '/' + str(ip_address)
+                ]
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as sub:
+        sub.wait()
+
+    command = ['ivre',
+                'db2view',
+                'nmap',
+                '--category',
+                'netflow'
+                ]
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as sub:
+        sub.wait()
+
 def NmapTCPScan(ip_version, ip_address, port_list):
     """
     TODO: fill this
@@ -47,8 +66,14 @@ def NmapTCPScan(ip_version, ip_address, port_list):
                 str(ip_address),
                 '-p',
                 port_list,
+                '--script',
+                'auth,malware,vuln',
                 '-oX',
-                '/tmp/FlowScanner/output_tcp_' + str(ip_address) + '.xml']
+                os.getenv('nmap_tmp_output_folder') + '/' + str(ip_address) + '/tcp.xml',
+                'ivre',
+                'scan2db',
+                '-c',
+                'service']
     if ip_version == "IPv6":
         command.append('-6')
     with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as sub:
@@ -65,8 +90,10 @@ def NmapUDPScan(ip_version, ip_address, port_list):
                 '-p',
                 port_list,
                 '-sU',
+                '--script',
+                'auth,malware,vuln',
                 '-oX',
-                '/tmp/FlowScanner/output_udp_' + str(ip_address) + '.xml',
+                os.getenv('nmap_tmp_output_folder') + '/' + str(ip_address) + '/udp.xml',
                 '--append-output']
     if ip_version == "IPv6":
         command.append('-6')
