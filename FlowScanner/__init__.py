bb513846fc2f2d8892295d96d9f2358fb9bfd480
Marvin Arets <mail@marvinarets.eu>
2022-04-07 16:29:53 +0200

    Add setup and binary

---
+++ FlowScanner/__init__.py
@@ -0,0 +1,12 @@
+"""
+FlowScanner
+"""
+
+#! /usr/bin/env python
+
+from typing import Optional
+
+def _version() -> Optional[str]:
+    return "V0.0.1"
+
+__version__ = VERSION = _version()
---
+++ Setup.py
@@ -0,0 +1,28 @@
+"""
+Standard setup.py file. Run
+$ python setup.py build
+# python setup.py install
+"""
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+VERSION = __import__("FlowScanner").VERSION
+
+setup(name='FlowScanner',
+      version=VERSION,
+      description='Processes NetFlow data into Nmap scans.',
+      author='Marvin Arets',
+      author_email='marvin.arets@surf.nl',
+      url='https://github.com/aretsmarvin/FlowScanner/',
+      packages=find_packages(include=['FlowScanner', 'FlowScanner.*']),
+      python_requires=">=3.10",
+      install_requires=[
+        "requests",
+        "python-dotenv",
+        "watchdog",
+        "netaddr",
+        "mysql-connector-python",
+        ],
+        scripts=['Bin/FlowScanner'],
+)
