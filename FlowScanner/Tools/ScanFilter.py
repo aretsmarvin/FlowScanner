e4c182f00de3221242caa7cff7adb12cc4ed88ae
Marvin Arets <mail@marvinarets.eu>
2022-05-20 13:28:39 +0200

    Remove ScanFilter class

--- FlowScanner/Tools/ScanFilter.py
+++ FlowScanner/Tools/ScanFilter.py
@@ -9,26 +9,21 @@ from datetime import datetime
 
 from FlowScanner.Database import MySQL
 
-class ScanFilter:
+@staticmethod
+def PortFilter(ip_address, port_list, proto):
     """
-    The FlowFilter class is responsible for filtering the server
-    IP's and port out of the flow data.
+    Function to filter recently scanned ports from IP.
     """
-    @staticmethod
-    def PortFilter(ip_address, port_list, proto):
-        """
-        Function to filter recently scanned ports from IP.
-        """
-        if port_list:
-            now = datetime.now()
-            loop_list = port_list.copy()
-            for port in loop_list:
-                last_scan_time = MySQL.GetLastScanTime(
-                            str(ip_address),
-                            port,
-                            proto)
-                if last_scan_time is not None:
-                    if (now - last_scan_time[0]).total_seconds() < int(
-                            os.getenv('min_scan_timeout_seconds', "3600")):
-                        port_list.remove(port)
-        return port_list
+    if port_list:
+        now = datetime.now()
+        loop_list = port_list.copy()
+        for port in loop_list:
+            last_scan_time = MySQL.GetLastScanTime(
+                        str(ip_address),
+                        port,
+                        proto)
+            if last_scan_time is not None:
+                if (now - last_scan_time[0]).total_seconds() < int(
+                        os.getenv('min_scan_timeout_seconds', "3600")):
+                    port_list.remove(port)
+    return port_list
